fa6b8f75643fab9501785a5a16487effdec0a932
khanhbq_he2002 <khanhbui1560@gmail.com>
2024-01-30 19:51:10 +0700

    Update rank logic

--- Assets/_Game/Scripts/UI/RankUI.cs
+++ Assets/_Game/Scripts/UI/RankUI.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using System;
 using TMPro;
+using System.Numerics;
+using Unity.Mathematics;
 
 public class RankUI : UIBase
 {
@@ -15,20 +17,110 @@ public class RankUI : UIBase
     [SerializeField] private RankBox _rankBoxPrefab;
     [SerializeField] private RankBox _myRank;
     [SerializeField] private Transform _rankBoxContainer;
-
+    private List<RankBox> rankBoxes;
+    private List<RankData> _globalRankDatas;
+    private List<RankData> _localRankDatas;
+    private int _randomSeed;
+    private bool _isLocalRank = true;
+    private int _localRank;
 
     private float _duration = 0.5f;
+    private void Awake()
+    {
+        CreateRankBox();
+    }
 
     void OnEnable()
     {
         _escapeButton.gameObject.SetActive(_isPopup);
+        CheckRankSeed();
     }
 
     void Start()
     {
         _switchBtn.onClick.AddListener(Switch);
         _escapeButton.onClick.AddListener(CloseRank);
-        StartCoroutine(GetDataFromApi());
+    }
+
+    private void CheckRankSeed()
+    {
+        int seed = DateTime.UtcNow.DayOfYear.GetHashCode();
+        bool hasConnection = CheckWiFiConnection();
+        if (hasConnection)
+        {
+            if (_randomSeed != seed)
+            {
+                _randomSeed = seed;
+                StartCoroutine(GetDataFromApi());
+            }
+            else
+                LoadPlayerData(hasConnection);
+        }
+        else
+            LoadPlayerData(hasConnection);
+
+    }
+
+    private void CreateRankBox()
+    {
+        rankBoxes = new List<RankBox>();
+        for (int i = 0; i < 10; i++)
+        {
+            RankBox rankBox = Instantiate(_rankBoxPrefab, _rankBoxContainer);
+            rankBox.Init((i + 1).ToString(), "", "", "");
+            rankBoxes.Add(rankBox);
+        }
+    }
+
+    private void LoadData()
+    {
+        if (_globalRankDatas == null || _localRankDatas == null) return;
+        if (_isLocalRank)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                rankBoxes[i].Init((i + 1).ToString(), _localRankDatas[i].name, BigNumber.FormatBack(_localRankDatas[i].score), _localRankDatas[i].country);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                rankBoxes[i].Init((i + 1).ToString(), _globalRankDatas[i].name, BigNumber.FormatBack(_globalRankDatas[i].score), _globalRankDatas[i].country);
+            }
+        }
+        LoadPlayerData(CheckWiFiConnection());
+    }
+
+    private void LoadPlayerData(bool hasConnection)
+    {
+        System.Random random = new System.Random(_randomSeed);
+        BigInteger playerScore = RuntimeDataManager.Instance.PlayerData.HighScore.String2BigInterger();
+        bool flag = (BigInteger.Compare(playerScore, 0) == 0 || !hasConnection);
+        if (_isLocalRank)
+        {
+            _localRank = random.Next(100, short.MaxValue);
+            _myRank.Init(flag ? "Unrank" : _localRank.ToString(), RuntimeDataManager.Instance.PlayerData.PlayerName, RuntimeDataManager.Instance.PlayerData.HighScore.String2Point(), "VN");
+        }
+        else
+        {
+            _myRank.Init(flag ? "Unrank" : random.Next(Mathf.RoundToInt((float)(_localRank * random.NextDouble())), 100000).ToString(), RuntimeDataManager.Instance.PlayerData.PlayerName, RuntimeDataManager.Instance.PlayerData.HighScore.String2Point(), "#VN");
+        }
+    }
+
+    private bool CheckWiFiConnection()
+    {
+        NetworkReachability reachability = Application.internetReachability;
+
+        switch (reachability)
+        {
+            case NetworkReachability.ReachableViaLocalAreaNetwork:
+                return true; // Connected via WiFi
+            case NetworkReachability.ReachableViaCarrierDataNetwork:
+                return true;
+            default:
+                return false; // Not connected via WiFi
+        }
     }
 
     private void OnHighScoreChange(string highScore)
@@ -42,6 +134,8 @@ public class RankUI : UIBase
         _toggleBtn.DOLocalMoveX(-_toggleBtn.localPosition.x, _duration)
             .OnComplete(() =>
             {
+                _isLocalRank = !_isLocalRank;
+                LoadData();
                 _switchBtn.interactable = true;
             });
     }
@@ -54,17 +148,13 @@ public class RankUI : UIBase
     private IEnumerator GetDataFromApi()
     {
         UIManager.Instance.StartLoading();
-        // Number of people in the leaderboard (10 in this case)
-        int leaderboardSize = 10;
 
         // Create a Random instance
         System.Random random = new System.Random(DateTime.UtcNow.DayOfYear.GetHashCode());
 
-        // Assuming you have a minimum and maximum score length
-        int minScoreLength = 10; // replace with your desired minimum score length
-        int maxScoreLength = 30; // replace with your desired maximum score length
-
-        using (UnityWebRequest www = UnityWebRequest.Get($"https://randomuser.me/api/?results={leaderboardSize}&inc=name,nat&seed=khanh"))
+        RankData[] globalRankData = new RankData[20];
+        int i = 0;
+        using (UnityWebRequest www = UnityWebRequest.Get($"https://randomuser.me/api/?results={10}&inc=name,nat&nat=us&seed=khanh"))
         {
             yield return www.SendWebRequest();
 
@@ -78,18 +168,55 @@ public class RankUI : UIBase
                 ApiResponse apiResponse = JsonUtility.FromJson<ApiResponse>(www.downloadHandler.text);
 
                 // Access the data
-                int i = 1;
                 foreach (Result result in apiResponse.results)
                 {
-                    string name = $"#{result.nat} {result.name.first} {result.name.last}";
+                    string name = $"{result.name.first} {result.name.last}";
                     Debug.Log(name);
-                    RankBox rankBox = Instantiate(_rankBoxPrefab, _rankBoxContainer);
-                    rankBox.Init(i++, name, GenerateRandomScore(minScoreLength, maxScoreLength, random).String2Point(), result.nat);
+                    globalRankData[i++] = new RankData(name, BigInteger.Parse(GenerateRandomScore(20, 40, random)), "#VN");
                     yield return null;
                 }
             }
         }
-        _myRank.Init(random.Next(100, short.MaxValue), RuntimeDataManager.Instance.PlayerData.PlayerName, RuntimeDataManager.Instance.PlayerData.HighScore.String2Point(), "VN");
+
+        using (UnityWebRequest www = UnityWebRequest.Get($"https://randomuser.me/api/?results={10}&inc=name,nat&seed=khanh"))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError("Error: " + www.error);
+            }
+            else
+            {
+                // Deserialize JSON response
+                ApiResponse apiResponse = JsonUtility.FromJson<ApiResponse>(www.downloadHandler.text);
+
+                // Access the data
+                foreach (Result result in apiResponse.results)
+                {
+                    string name = $"{result.name.first} {result.name.last}";
+                    Debug.Log(name);
+                    globalRankData[i++] = new RankData(name, BigInteger.Parse(GenerateRandomScore(30, 60, random)), "#" + result.nat);
+                    yield return null;
+                }
+            }
+        }
+
+        Array.Sort(globalRankData, (x, y) => BigInteger.Compare(y.score, x.score));
+        _globalRankDatas = new List<RankData>(globalRankData);
+        _globalRankDatas.AddRange(globalRankData);
+        _localRankDatas = new List<RankData>();
+        foreach (RankData data in _globalRankDatas)
+        {
+            if (data.country == "#VN")
+            {
+                _localRankDatas.Add(data);
+            }
+            yield return null;
+        }
+
+        LoadData();
+
         UIManager.Instance.StopLoading();
     }
 
--- Assets/_Game/Scripts/UIElements/RankBox.cs
+++ Assets/_Game/Scripts/UIElements/RankBox.cs
@@ -8,9 +8,9 @@ public class RankBox : MonoBehaviour
     [SerializeField] private TMP_Text _score;
     [SerializeField] private TMP_Text _country;
 
-    public void Init(int rank, string name, string score, string country)
+    public void Init(string rank, string name, string score, string country)
     {
-        _rank.text = rank.ToString();
+        _rank.text = rank;
         _name.text = name;
         _score.text = score;
         _country.text = country;
