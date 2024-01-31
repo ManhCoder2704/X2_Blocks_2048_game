dfe81c531d7eda614553244eef45002ac828a9c6
Mark Nguyễn <manh9403@gmail.com>
2024-01-31 15:56:49 +0700

    alo

--- Assets/_Game/Scripts/UI/SettingUI.cs
+++ Assets/_Game/Scripts/UI/SettingUI.cs
@@ -6,20 +6,29 @@ public class SettingUI : UIBase
 {
     [SerializeField] private Button _vibraBtn;
     [SerializeField] private Button _musicBtn;
+    [SerializeField] private IconController _musicIcon;
+    [SerializeField] private IconController _vibraIcon;
 
     void Awake()
     {
         _vibraBtn.onClick.AddListener(OnVibration);
         _musicBtn.onClick.AddListener(OnMusic);
     }
+    private void OnEnable()
+    {
+        _musicIcon.Swap(RuntimeDataManager.Instance.SettingData.IsSoundOn);
+        _vibraIcon.Swap(RuntimeDataManager.Instance.SettingData.IsVibrationOn);
+    }
 
     private void OnMusic()
     {
+        _musicIcon.Swap(RuntimeDataManager.Instance.SettingData.IsSoundOn);
         SoundManager.Instance.ChangeSoundable();
     }
 
     private void OnVibration()
     {
+        _vibraIcon.Swap(RuntimeDataManager.Instance.SettingData.IsVibrationOn);
         SoundManager.Instance.ChangeVibratable();
     }
 }
